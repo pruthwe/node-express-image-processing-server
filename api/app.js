8be6aae20b29d192506af55c727c9c5669c180b4
pruthwe <indragantipruthviraj@gmail.com>
2022-12-22 00:04:51 +0530

    implemented storing image on disk

--- api/app.js
+++ api/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const router = require("./src/router");
 const path = require("path");
 
 const pathToIndex = path.resolve(__dirname, "../client/index.html");
 const app = express();
+app.use("/", router);
+app.use(express.static(path.resolve(__dirname, "uploads")));
 app.use("/*", (request, response) => {
     response.sendFile(pathToIndex);
 });
